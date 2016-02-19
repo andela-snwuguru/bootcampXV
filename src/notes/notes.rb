8bc4d744549df1496e593c80bd814788908cd0e0
Sunday Goodman <sundayguru@gmail.com>
2016-02-19 21:44:40 +0100

    Implement the following methods list, get,delete, edit and added two helper methods print and printLine

--- src/notes/notes.rb
+++ src/notes/notes.rb
@@ -7,30 +7,54 @@ class NotesApplication
 
 	def create note_content
 		return nil if note_content.length == 0
-		@notes << {author: @author,note: note_content}
+		@notes << {author: @author,note: note_content,id: @notes.length}
 		@notes.length - 1
 	end
 
 	def list
-
+		@notes.each{ |note|
+			print note
+		}
+		
 	end
 
 	def get note_id
-
+		return @notes[note_id] if @notes[note_id] != nil
+		'Invalid Note ID'
 	end
 
 	def search search_text
 		
 	end
 
-	def delete
-
+	def delete note_id
+		@notes.delete_at(note_id) if @notes[note_id] != nil
 	end
 
-	def edit
+	def edit note_id, note_content
+		return false if @notes[note_id] == nil || note_content.length == 0
+		@notes[note_id] = {author: @author,note: note_content,id:note_id}
+		true
+	end
 
+	
+	def print note
+		return puts 'Invalid note records' if !note.is_a? Hash
+		puts "Note ID: #{note[:id]}"
+		puts "#{note[:note]}"
+		puts "By Author #{note[:author]}"
+		printLine
+	end
+
+	def printLine
+		puts "--------------------------------------------------------------------"
 	end
 end
 
 notes = NotesApplication.new 'Sunday'   
-puts notes.create 'testing note'
+notes.create 'testing note'
+notes.create 'test 3'
+notes.create 'test 4'
+notes.list
+notes.edit 2, 'just modified'
+notes.list
